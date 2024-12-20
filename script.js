b047bc5eddf5d74544cd2d315e6cb532b511e41b
xrxshii <dashmir2008@outlook.com>
2024-12-20 23:25:04 +0100

    Update script.js
    adding a local storage to save the results

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-
-let result = '';
-let computerResult = '';
-const gameStats = {
+// this retrieves the data previously saved from the browsers local data || if the local storage has no data saved it returns 0
+let gameStats = (JSON.parse(localStorage.getItem('gameStats'))) || {
   win: 0,
   lose: 0
 };
+
+let result = '';
+let computerResult = '';
+
 
 // decides the game result for the player
 function decidingResult (playerPick, resetButton) {
@@ -17,6 +19,9 @@ function decidingResult (playerPick, resetButton) {
     gameStats.win +=1;
   } 
   
+  // saving the game Stats local, with JSON.stringify we can turn the gameStats number into a string 
+  localStorage.setItem('gameStats', JSON.stringify(gameStats));
+
   // text under the buttons
   document.getElementById('textDisplay0').textContent = (`You picked ${playerPick}. The coin is ${computerResult}.`);
   document.getElementById('textDisplay1').textContent = (`You ${result}`)
